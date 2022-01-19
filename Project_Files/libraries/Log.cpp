b6b4a970eb0c115bec6fc8a663d9fbae89552cb3
JulienIV <2001lorenzoleone@gmail.com>
2022-01-19 18:24:12 +0100

    Log.cpp
    Ho terminato l'implementazione di Log (cioè ho scritto le funzioni a mio parere necessarie per Schacchiera.cpp e Replay.cpp)

--- Project_Files/libraries/Log.cpp
+++ Project_Files/libraries/Log.cpp
@@ -1,7 +1,7 @@
 /*  Lorenzo Leone 2000160  */
 
 #include "Log.h"
-
+#include <string>
 //--------------------------------------------------------| Costruttori e distruttore |--------------------------------------------------------
 Log::Log()
 {
@@ -26,29 +26,76 @@ Log::Log(std::string s)
 */
 void Log::write_xy(std::string s, Position a, Position b)
 {   
-    int letter_a = a.get_letter(); 
-    int number_a = a.get_number();
+    int letter_a = a.get_letter() + 97;
+    int number_a = a.get_number() + 1;
     std::string pos_in = "" + letter_a + number_a;
 
-    int letter_b = b.get_letter();
-    int number_b = b.get_number();
+    int letter_b = b.get_letter() + 97;
+    int number_b = b.get_number() + 1;
     std::string pos_fin = "" + letter_b + number_a;
 
-    std::string move = pos_in + "-" + pos_fin + "\n"; //ogni mossa inserita in s.txt avrà forma a-b (es. A1-B5)
-
-    file.open(s, std::ios::out | std::ios::app);
+    file.open(s, std::ios::out | std::ios::app); //write in modalità append
     /*
         _> ios::out, indica che s.txt verrà aperto in modalità scrittura.
         _> ios::app, indica che ciò che viene scritto verrà aggiunto in seguito all'ultima modifica eseguita in s.txt.
         _> il simbolo | combina le modalità di apertura file.
     */ 
-
-    file << move;
+    if(file.is_open())
+    {
+        /*
+            nel file le coodinate saranno salvate:
+            pos_in\n
+            pos_fin\n
+            pos_in\n
+            pos_fin\n
+            ...
+        */
+        file << pos_in + "\n";
+        file << pos_fin + "\n";
+    }
     file.close();
 }
-/*
+
 std::vector<Position> Log::read_xy(std::string s)
 {
-    file.open(s, std::ios::in);
+    
+    int letter;
+    int number;
+    Position temp;
+    std::string pos_str;
+    std::vector<Position> move;
+
+    file.open(s, std::ios::in); //read
+    if (!file.eof() && file.is_open())  //file.eof() restituisce true, se nel file sono finiti i dati da leggere.
+    {
+        /*
+            uso un for che si ripete due volte perchè
+            nel file le coodinate sono salvate:
+            pos_in\n
+            pos_fin\n   (prima mossa)
+            pos_in\n
+            pos_fin\n   (seconda mossa)
+            ...
+        */
+        for(int i = 0; i < 2; i++) 
+        {
+            getline(file, pos_str);
+            letter = pos_str[0] - 97;
+            number = pos_str[1] - 1;
+            temp.set_position(letter, number);
+            move.push_back(temp);
+            //al primo ciclo, salva pos_in, al secondo, salva pos_fin
+        }
+        file.close();
+        return  move; //move contiene un coppia di posizioni, move[0] = pos_in, move[1] = pos_fin.
+    }
+    else if(!file.is_open())
+    {
+        std::cout<<"ERRORE: file non aperto per l'acquisizione dati (controlla read_xy in log.cpp)\n";
+    }  
+    else
+    {
+        std::cout<<"Dati acquisibili dal file " << s << " terminati!";
+        file.close();     
+    }
 }
-*/
