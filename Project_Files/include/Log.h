03bd321795ae14de9bd77d68551cb039ce231e78
JulienIV <2001lorenzoleone@gmail.com>
2022-01-21 18:22:52 +0100

    FINITO, forse, il Commisar Zlatko né sarà giudice ed esecutore
    Ho finito log.h/log.cpp e Replay, sperando di aver soddisfatto i requisiti.
    
    Non ci sono errori e compila.
    
    Non so se però funzioni :D

--- Project_Files/include/Log.h
+++ Project_Files/include/Log.h
@@ -7,7 +7,7 @@
             corrisponde al file txt su cui verranno scritte le mosse eseguite durante la partita
 
     std::string title :
-            titolo del file txt
+            titolo del file txt (scrivere la directory del file.txt)
 
 --------| COSTRUTTORI |------------------------------------------------
     Log() :
@@ -17,21 +17,41 @@
             costruttore che crea un file txt con titolo custom (inserito a parametro)
 
 --------| FUNZIONI |---------------------------------------------------
-    write_xy(std::string s, Position a, Position b) :
+    write_file(Log mt, Chessboard& cb, Position a, Position b) :
             funzione che ha come parametri
-                1) std::string s : il titolo del txt su cui salvare le mosse
+                1) Log mt : oggetto log, in cui è specificato il titolo del file.txt
+                2) Chessboard& cb: oggetto chessboard, necessario per l'uso di certe funzioni interne (come w_move_type)
                 2) Position a : posizione iniziale della pedina da muovere
                 3) Position b : posizione che la pedina occuperà alla fine del turno
             
-            con questi valori, creo un stringa sul file "s.txt" del tipo "a-b" (es. "A1-B5"), dove le lettere
-            sono espresse in codifica ASCII.
+            con questi valori, creo un stringa sul file "s.txt" del tipo "A1B50"), dove i primi due
+            caratteri solo la posizione iniziale, il terzo e quarto la posizione finale, e il quinto è
+            il tipo di mossa effettuata, assegnata da w_move_type (write_move_type per esteso).
 
-    read_xy(std::string s) :
-            funzione che restituisce una mossa a chiamata in ordine cronologico d'inserimento.
+    read_file(std::string s) :
+            Partendo dal file.txt, estrae ogni mossa in forma di un vettore di int, che da solo non fa nulla, necessita di get_xy() e spec_type_move()
 
     file_eof(std::string t) :
             funzione che restituisce true quando sono finiti gli elementi da acquisire del file
 
+    get_xy(std::vector<int> move) :
+            parte dal vettore restituito da read_file() e restituisce a sua volta un vettore di posizione che contiene 
+            due posizioni, quella iniziale e finale della mossa effettuata
+
+    w_move_type(Chessboard& cb, Position a, Position b) :
+            funzione usata all'interno di write_file(), con il compito di filtrare e assegnare che tipo di mossa
+            è stata effettuata attraverso una serie di if.
+            Restituisce un numero int:
+            - 0 : la mossa porta alla vittoria dei pezzi neri;
+            - 1 : la mossa porta alla situazione di patta;
+            - 2 : la mossa porta alla vittoria dei pezzi bianchi;
+            - 3 : la mossa è un semplice spostamento;
+            - 4 : la mossa porta una pedina a "mangiare" una avversaria.
+    
+    spec_type_move(Chessboard& cb, std::vector<int> move) :
+            funzione simile a w_move_type, usata però per i commenti dopo una mossa,
+            in base allo schema soprastante, scrive su terminale cosa è successo dopo ogni mossa.
+            Esegue anche un "Assegnamento testuale" alle diverse informazioni di una pedina.
 
 
 */
@@ -46,13 +66,13 @@ class Log
 {
     public:
         Log();
-        ~Log();
         Log(std::string t);
         bool file_eof(std::string t);
         std::vector<int> read_file(std::string t); 
-        std::vector<Position> get_xy(std::string t);
+        std::vector<Position> get_xy(std::vector<int> move);
         int w_move_type(Chessboard& cb, Position a, Position b);
-        void write_file(std::string t, Chessboard& cb, Position a, Position b); 
+        void write_file(Log mt, Chessboard& cb, Position a, Position b); 
+        void spec_type_move(Chessboard& cb, std::vector<int> move);
        
     private:
         std::fstream file;
